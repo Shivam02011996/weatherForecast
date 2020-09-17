cd51abe84c4563f49960d753c32779f762d7b12b
Rahul Rohilla <rahul.kumar13@mountblue.tech>
2020-09-17 14:53:55 +0530

    imported header and displaying to loading page

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './LoadingPage.css'
+import Header from './header/Header'
 
 export default function LoadingPage(){
   return (
 		<div className="bg-image">
-        started
+        <Header />
     </div>  );
 }
