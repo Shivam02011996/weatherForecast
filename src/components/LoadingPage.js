3e64a7f8c9e71e04d7d53561dbbd76fb7952d4e3
Rahul Rohilla <rahul.kumar13@mountblue.tech>
2020-09-18 01:40:56 +0530

    imported main component and loaded to display

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './LoadingPage.css'
 import Header from './header/Header'
+import MainComponent from './homepage/main-component/MainComponent'
 
 export default function LoadingPage(){
   return (
 		<div className="bg-image">
         <Header />
+        <MainComponent />
+
     </div>  );
 }
