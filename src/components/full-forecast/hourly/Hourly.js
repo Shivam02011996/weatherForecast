5a45d85fa6943bda6233a0d3aa673bb8506195ac
Rahul Rohilla <rahul.kumar13@mountblue.tech>
2020-09-20 17:44:13 +0530

    added newly created component and removed old component and styled again

--- src/components/full-forecast/hourly/Hourly.js
+++ src/components/full-forecast/hourly/Hourly.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import Interval from '../interval/Interval'
 import './Hourly.css'
+import WeatherTable from './../five-day/WeatherTable'
 
 export default function Hourly(props){
   
  let allDataRows = props.fullWeatherForecasts.list;
- let topTwoRows = [];
-  for(let i = 0; i < 2; i++){
-    topTwoRows.push(allDataRows[i]);
-  }
+
+  let locatedCity = {
+      locatedCity: props.fullWeatherForecasts.city
+    }
 
 
 
   return (
+
     <div className="hourly-card">
-      <Interval 
-      interval={topTwoRows[0]}
-      heading="Now"
-      />
-      <Interval 
-      interval={topTwoRows[1]}
-      heading="After 3-hour"
-      />     
+        <WeatherTable 
+          fullWeatherForecasts= {allDataRows}
+          city={locatedCity}
+          isFullDateShow={true}
+          />
+
+      
       </div>
   );
 }
