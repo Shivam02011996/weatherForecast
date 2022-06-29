f38cbffb4ebc2984b865cc37189e9f30572a471a
Shivam Mishra <shivammishra0002@gmail.com>
2022-06-29 11:41:24 +0530

    weatherForeCast

--- src/components/full-forecast/environment-property/EnvironmentProperty.js
+++ src/components/full-forecast/environment-property/EnvironmentProperty.js
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 
 export default function EnvironmentCondition(props){
   return (<div className="environment-card">
-    <div className="environment-heading"><strong>{props.heading}</strong></div>
+    <div className="environment-heAding"><strong>{props.heading}</strong></div>
     <div className="environment-icon"><Icon icon={props.icon} /></div>
     <div className="environment-value">{props.value}{props.measure}</div>
   </div>);
